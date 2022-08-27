98baa574ffdef0e1c6e85cb412eda5d53004c1e0
Andrew Stahl <andrewstahl96@gmail.com>
2022-08-27 18:29:09 -0400

    commit tree 6c84c41b1916995b085f051e37c42bb08c5d8525

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,22 @@
+require 'pry'
 # Build a class EmailAddressParser that accepts a string of unformatted 
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+class EmailAddressParser
+  attr_reader :parse
+  attr_reader :email_string
+
+  def initialize(email_string)
+    @email_string = email_string
+  end
+
+  def parse
+    email_array = email_string.split(/[, ]+/)
+    # Returns the unique values of the array
+    email_array.uniq
+  end
+
+  binding.pry
+
+end
